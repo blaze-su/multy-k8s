b6637aa70131d4e34267faa6fefc6c655dab7a98
Aleksandr Sidorov <a.sidorov@blaze.su>
2019-12-20 00:29:25 +0300

    add deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t sidorov/multi-client:latest -t sidorov/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sidorov/multi-server:latest -t sidorov/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sidorov/multi-worker:latest -t sidorov/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push sidorov/multi-client:latest
+docker push sidorov/multi-server:latest
+docker push sidorov/multi-worker:latest
+
+docker push sidorov/multi-client:$SHA
+docker push sidorov/multi-server:$SHA
+docker push sidorov/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set imege deployments/client-deployment client=sidorov/multi-client:$SHA
+kubectl set imege deployments/server-deployment server=sidorov/multi-server:$SHA
+kubectl set imege deployments/worker-deployment worker=sidorov/multi-worker:$SHA
